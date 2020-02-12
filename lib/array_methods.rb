79dcb14ada111f7759eaa189093ceea01daf6c3f
Brett Bedevian <brettbedevian@gmail.com>
2020-02-12 15:30:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,12 @@
 # end
 
 def find_element_index(array, value_to_find)
-  
+   array.length.times { |i|
+  if array[i] == value_to_find 
+    return array[i]
+  end
+  } 
+  nil 
 end
 
 def find_max_value(array)
